31eaf3ee1492112de633967aaf7700486f0753b3
Craig Thacker <craigthackerx@gmail.com>
2025-05-03 23:35:08 +0100

    Update code

--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
-variable "working_dir" {
-  type        = string
-  description = "The working directory for the module"
-  default     = null
-}
-
 variable "linux_timestamp_format" {
-  type = string
+  type        = string
   description = "The format of the timestamp to generate on Linux"
   default     = "+%d-%m-%Y:%H:%M"
 }
 
 variable "windows_timestamp_format" {
-  type = string
+  type        = string
   description = "The format of the timestamp to generate on Windows"
   default     = "dd-MM-yyyy:HH:mm"
+}
+
+variable "working_dir" {
+  type        = string
+  description = "The working directory for the module"
+  default     = null
 }
